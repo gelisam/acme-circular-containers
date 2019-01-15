fb0d3375e266f4a1b8008f2f0433a17ae12d3aa1
Samuel Gélineau <gelisam+github@gmail.com>
2019-01-15 10:11:58 -0500

    Data.Tree.Circular.freeze

--- src/Data/Tree/Circular.hs
+++ src/Data/Tree/Circular.hs
@@ -1,12 +1,26 @@
+{-# LANGUAGE RecordWildCards, ScopedTypeVariables #-}
 module Data.Tree.Circular where
 
 import qualified Data.Tree as Containers
 
 
-data Tree a
+data Tree a = Node
+  { treeParent   :: Maybe (Tree a)
+  , treeLabel    :: a
+  , treeChildren :: [Tree a]
+  }
 
-freeze :: Containers.Tree a -> Tree a
-freeze = error "not implemented yet"
+freeze :: forall a. Containers.Tree a -> Tree a
+freeze = go Nothing
+  where
+    go :: Maybe (Tree a) -> Containers.Tree a -> Tree a
+    go parent (Containers.Node a ts) = tree
+      where
+        tree :: Tree a
+        tree = Node parent a children
+
+        children :: [Tree a]
+        children = go (Just tree) <$> ts
 
 thaw :: Tree a -> Containers.Tree a
 thaw = error "not implemented yet"
